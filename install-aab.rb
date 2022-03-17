209e15961459a8300ca5e1863779ec90bf811fa2
Igor Ferreira <me@igorcferreira.dev>
2022-03-17 23:09:19 +0100

    Update install-aab version

--- install-aab.rb
+++ install-aab.rb
@@ -1,34 +1,18 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class InstallAab < Formula
-  desc "Basic bash script that can be used to install AAB files into connected Android devices/emulators"
+  desc "Bash script that can be used to install AAB files into connected Android devices"
   homepage "https://github.com/igorcferreira/install-aab"
+  url "https://github.com/igorcferreira/install-aab/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "596c3445a72af1e7c5ef1ce0bd85aba9b05ecfc4e7f41f1f0f6bfe1d878213cf"
   license "Apache-2.0"
-  sha256 "507901881669ec13b7254f55a5d61b2e17a0c7ca7d68453bb8db23c0ec6bf560"
-  url "https://github.com/igorcferreira/install-aab/archive/refs/tags/1.0.0.tar.gz"
   head "https://github.com/igorcferreira/install-aab.git", branch: "main"
 
-  depends_on "openjdk"
   depends_on "bundletool"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    bin.install 'install-aab'
+    bin.install "install-aab"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test install-aab`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
