9b3c5fb58076ddc6ee9d8a918e301ce75bc0ccf2
Igor Castañeda Ferreira <me@igorcferreira.dev>
2022-05-04 18:09:12 +0200

    Update install-aab to use release

--- install-aab.rb
+++ install-aab.rb
@@ -5,8 +5,8 @@
 class InstallAab < Formula
   desc "Bash script that can be used to install AAB files into connected Android devices"
   homepage "https://github.com/igorcferreira/install-aab"
-  url "https://github.com/igorcferreira/install-aab/archive/refs/tags/1.0.1.tar.gz"
-  sha256 "596c3445a72af1e7c5ef1ce0bd85aba9b05ecfc4e7f41f1f0f6bfe1d878213cf"
+  url "https://github.com/igorcferreira/install-aab/releases/download/1.0.1/install-aab"
+  sha256 "85e16577b5954a7b9bac74a38d14270a3c1e2fab7a8fcfe164d92cdfb28938b7"
   license "Apache-2.0"
   head "https://github.com/igorcferreira/install-aab.git", branch: "main"
 
