6957db7eccb30036c5eccf2c3ac0603270458d42
Igor Castañeda Ferreira <me@igorcferreira.dev>
2022-05-04 18:34:59 +0200

    Update licenses

--- install-aab.rb
+++ install-aab.rb
@@ -5,10 +5,10 @@
 class InstallAab < Formula
   desc "Bash script that can be used to install AAB files into connected Android devices"
   homepage "https://github.com/igorcferreira/install-aab"
-  url "https://github.com/igorcferreira/install-aab/releases/download/1.0.1/install-aab"
+  url "https://github.com/igorcferreira/install-aab/releases/download/1.0.2/install-aab"
   sha256 "85e16577b5954a7b9bac74a38d14270a3c1e2fab7a8fcfe164d92cdfb28938b7"
-  license "Apache-2.0"
-  head "https://github.com/igorcferreira/install-aab.git", tag: "1.0.1"
+  license "MIT"
+  head "https://github.com/igorcferreira/install-aab.git", tag: "1.0.2"
 
   depends_on "bundletool"
 
