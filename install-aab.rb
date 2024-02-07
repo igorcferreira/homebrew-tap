1365b776345316fcbe6e611d64ca4116382fa5b0
Igor Ferreira <me@igorcferreira.dev>
2024-02-07 17:04:49 +0100

    Update install-aab tap

--- install-aab.rb
+++ install-aab.rb
@@ -5,10 +5,10 @@
 class InstallAab < Formula
   desc "Bash script that can be used to install AAB files into connected Android devices"
   homepage "https://github.com/igorcferreira/install-aab"
-  url "https://github.com/igorcferreira/install-aab/releases/download/1.0.2/install-aab"
-  sha256 "85e16577b5954a7b9bac74a38d14270a3c1e2fab7a8fcfe164d92cdfb28938b7"
+  url "https://github.com/igorcferreira/install-aab/releases/download/1.1.0/install-aab"
+  sha256 "44feaa50d24462dcf6826c855c825ab4492c28ab3c3b7d667888f096c57e6b9b"
   license "MIT"
-  head "https://github.com/igorcferreira/install-aab.git", tag: "1.0.2"
+  head "https://github.com/igorcferreira/install-aab.git", tag: "1.1.0"
 
   depends_on "bundletool"
 
