cda6c5e118111bb686c38a948005db9dc955c762
Igor Castañeda Ferreira <igorcferreira@users.noreply.github.com>
2025-03-23 01:09:40 +0100

    Create new Cask formula

---
+++ musicstreamsync.rb
@@ -0,0 +1,28 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "musicstreamsync" do
+    version "1.0.2"
+    sha256 "81081f99177f5cc4eda4a36481320681c1acdedeb6d8e215938177f3a2aa1a2f"
+
+    url "https://github.com/igorcferreira/MusicStreamSync/releases/download/#{version}/MusicStreamSync.zip",
+        verified: "github.com/igorcferreira/MusicStreamSync/"
+    name "MusicStreamSync"
+    desc "App that can be used to sync App Music plays into Last.FM"
+    homepage "https://github.com/igorcferreira"
+
+    # Documentation: https://docs.brew.sh/Brew-Livecheck
+    livecheck do
+        url :url
+        strategy :github_latest
+    end
+
+    app "MusicStreamSync.app"
+
+    # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+    zap trash: [
+        "~/Library/Application Support/MusicStreamSync",
+        "~/Library/Logs/MusicStreamSync",
+    ]
+end
+  
