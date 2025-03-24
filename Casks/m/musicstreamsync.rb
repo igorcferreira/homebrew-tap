d638baeb482de654f1c6d729eedfa092e0d4fb37
Igor Castañeda Ferreira <igorcferreira@users.noreply.github.com>
2025-03-24 18:04:30 +0100

    Update to v1.0.8

--- Casks/m/musicstreamsync.rb
+++ Casks/m/musicstreamsync.rb
@@ -2,8 +2,8 @@
 #                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "musicstreamsync" do
-    version "1.0.7"
-    sha256 "4836bc3ddfcde9fd57babe11bbb4230e69dacd15efa0916ec2dd241a7c84aea4"
+    version "1.0.8"
+    sha256 "ea2dd943d4915f8ba67b6a8cff2db50501ee6f99be6af0a855d9258967388489"
 
     url "https://github.com/igorcferreira/MusicStreamSync/releases/download/#{version}/MusicStreamSync.zip",
         verified: "github.com/igorcferreira/MusicStreamSync/"
