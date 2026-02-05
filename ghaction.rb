74827856f10c39a74f9e21874641d18ba458d5d2
Igor Ferreira <me@igorcferreira.dev>
2026-02-05 20:17:09 +0100

    ghaction [v1.0.1]

--- ghaction.rb
+++ ghaction.rb
@@ -5,10 +5,10 @@
 class Ghaction < Formula
   desc "CLI tool to trigger and monitor GitHub Actions workflows"
   homepage "https://github.com/igorcferreira/GHActionTrigger"
-  url "https://github.com/igorcferreira/GHActionTrigger/releases/download/v1.0.0/ghaction-v1.0.0-macos-universal.tar.gz"
-  sha256 "0c89166db2b69abfc216e8d4c9ed00fab41b17054c86f6a4a790bee0458f50a3"
+  url "https://github.com/igorcferreira/GHActionTrigger/releases/download/v1.0.1/ghaction-v1.0.1-macos-universal.tar.gz"
+  sha256 "09c02b8a12b3f0779e260a9df1c5f9a790a95a4a8d719bb8a10a88f1265ea33e"
   license "MIT"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "ghaction"
